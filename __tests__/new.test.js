bfb183669487d35fe2dc820355eec20ab58faf30
Missael Ortiz <ortiz.missael80@gmail.com>
2021-06-17 18:07:50 -0700

    ahhhhhhhh

--- __tests__/new.test.js
+++ __tests__/new.test.js
@@ -6,72 +6,6 @@ const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
 
-const magicItems =  [
-  {
-    id: 1,
-    name: 'Sword of Embers',
-    type: 'Sword',
-    level: 6,
-    cursed: false,
-    effect: 'A balde roughly forged with special metals. It lets off heat as if it was just taken out of the forge.'
-  },
-  {
-    id: 2,
-    name: 'Lizard of Warding',
-    type: 'Trinket',
-    level: 2,
-    cursed: false,
-    effect: 'A desicated lizard preserved with pungent herbs. It seems to take on curses easily.'
-  },
-  {
-    id: 3,
-    name: 'Lucky Shield',
-    type: 'Shield',
-    level: 3,
-    cursed: true,
-    effect: 'A shield that is not so lucky. Attacks seem to slip right past the shield and onto the arm.'
-  },
-  {
-    id: 4,
-    name: 'Ring of Feathers',
-    type: 'Ring',
-    level: 3,
-    cursed: false,
-    effect: 'A silver ring with a feather pattern. Makes you feel much lighter, and fall slower.'
-  },
-  {
-    id: 5,
-    name: 'Bracers of Storms',
-    type: 'Bracers',
-    level: 9,
-    cursed: false,
-    effect: 'A set of bracers made of a reflective, opalescent metal. They hum with an innate power, and often lance out bolts of lightning.'
-  },
-  {
-    id: 6,
-    name: 'Bag of Storing',
-    type: 'Misc',
-    level: 3,
-    cursed: true,
-    effect: 'This bag is bigger on the inside than it is on the outside. Any object that can fit inside the mouth of the bag are stored inside, but cannot be retrieved.'
-  },
-  {
-    id: 7,
-    name: 'Necklace of Keys',
-    type: 'Necklace',
-    level: 7,
-    cursed: true,
-    effect: 'A leather chord with a large amount of keys strung upon it. Any key that is used from this necklace will jam and permanently break any lock attempted to be used in.'
-  },
-  {
-    id: 8,
-    name: 'The Second Ring',
-    type: 'Ring',
-    level: 14,
-    cursed: true,
-    effect: 'A copy of the master ring. Super cursed.'
-  },
-];
 describe('post put and delete routes', () => {
   describe('routes', () => {
     let token;
@@ -96,73 +30,6 @@ describe('post put and delete routes', () => {
     afterAll(done => {
       return client.end(done);
     });
-
-    const magicItems =  [
-      {
-        id: 1,
-        name: 'Sword of Embers',
-        type: 'Sword',
-        level: 6,
-        cursed: false,
-        effect: 'A balde roughly forged with special metals. It lets off heat as if it was just taken out of the forge.'
-      },
-      {
-        id: 2,
-        name: 'Lizard of Warding',
-        type: 'Trinket',
-        level: 2,
-        cursed: false,
-        effect: 'A desicated lizard preserved with pungent herbs. It seems to take on curses easily.'
-      },
-      {
-        id: 3,
-        name: 'Lucky Shield',
-        type: 'Shield',
-        level: 3,
-        cursed: true,
-        effect: 'A shield that is not so lucky. Attacks seem to slip right past the shield and onto the arm.'
-      },
-      {
-        id: 4,
-        name: 'Ring of Feathers',
-        type: 'Ring',
-        level: 3,
-        cursed: false,
-        effect: 'A silver ring with a feather pattern. Makes you feel much lighter, and fall slower.'
-      },
-      {
-        id: 5,
-        name: 'Bracers of Storms',
-        type: 'Bracers',
-        level: 9,
-        cursed: false,
-        effect: 'A set of bracers made of a reflective, opalescent metal. They hum with an innate power, and often lance out bolts of lightning.'
-      },
-      {
-        id: 6,
-        name: 'Bag of Storing',
-        type: 'Misc',
-        level: 3,
-        cursed: true,
-        effect: 'This bag is bigger on the inside than it is on the outside. Any object that can fit inside the mouth of the bag are stored inside, but cannot be retrieved.'
-      },
-      {
-        id: 7,
-        name: 'Necklace of Keys',
-        type: 'Necklace',
-        level: 7,
-        cursed: true,
-        effect: 'A leather chord with a large amount of keys strung upon it. Any key that is used from this necklace will jam and permanently break any lock attempted to be used in.'
-      },
-      {
-        id: 8,
-        name: 'The Second Ring',
-        type: 'Ring',
-        level: 14,
-        cursed: true,
-        effect: 'A copy of the master ring. Super cursed.'
-      },
-    ];
       
     test('/POST magic-items creates a single magic item', async() => {
 
@@ -203,29 +70,29 @@ describe('post put and delete routes', () => {
 
       // make a request to update the new board game
       const data = await fakeRequest(app)
-        .put('/magicItems/5')
+        .put('/magicItems/9')
         .send({
-          name: 'New Bracers of Storms',
-          type: 'Bracers',
-          level: 10,
-          cursed: false,
-          effect: 'A set of bracers made of a reflective, opalescent metal. They hum with an innate power, and often lance out bolts of lightning. Now with more lightning!',
+          name: 'New New Sword',
+          type: 'Sword',
+          level: 2,
+          cursed: true,
+          effect: 'does more stuff',
         })
         .expect('Content-Type', /json/)
         .expect(200);
   
       const dataMagicItems = await fakeRequest(app)
-        .get('/magicItems/5')
+        .get('/magicItems')
         .expect('Content-Type', /json/)
         .expect(200);
   
       const newMagicItem = { 
-        'name': 'New Bracers of Storms',
-        'type': 'Bracers', 
-        'level': 10, 
-        'id': 5,
-        'cursed': false, 
-        'effect': 'A set of bracers made of a reflective, opalescent metal. They hum with an innate power, and often lance out bolts of lightning. Now with more lightning!', 
+        'name': 'New New Sword',
+        'type': 'Sword', 
+        'level': 2, 
+        'id': 9,
+        'cursed': true, 
+        'effect': 'does more stuff', 
         'owner_id': 1,
       };
         
